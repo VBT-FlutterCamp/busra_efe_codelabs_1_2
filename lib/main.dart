03c9733685840f8c2bb77ac62554395d080d3d4a
busraefee <busra.efee34@gmail.com>
2022-02-15 20:03:29 +0300

    codelabs first and second

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:startup_namer/random_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: RandomWords(),
+    );
+  }
+}
